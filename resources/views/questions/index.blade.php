049d9376b9396a390bcc85534a1d75fab94b76e3
nahoang <nahoang9698@gmail.com>
2023-05-24 23:36:37 +0700

    wip

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -9,7 +9,17 @@
 
                 <div class="card-body">
                     @foreach ($questions as $question)
-                        <div class="media">
+                          <div class="d-flex flex-column counters">
+                            <div class="vote">
+                              <strong>{{ $question->votes }}</strong> {{ str_plural('vote', $question->votes) }}
+                            </div>
+                            <div class="status">
+                              <strong>{{ $question->answers }}</strong> {{ str_plural('answers', $question->answers) }}
+                            </div>
+                            <div class="view">
+                              {{ $question->views . " " . str_plural('views', $question->views) }}
+                            </div>
+                          </div>
                           <div class="media-body">
                             <h3 class="mt-0">
                               <a href="{{ $question->url }}"> {{ $question->title }} </a>
@@ -20,7 +30,7 @@
                               <small class="text-muted">{{ $question->created_date }}</small>
                             </p>
                             {{ str_limit($question->body, 250) }}
-                          </div>
+                      
                         </div>
                         <hr>
                     @endforeach
