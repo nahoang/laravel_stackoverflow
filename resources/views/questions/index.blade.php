8b86609b683916660a4627dceb3a62c73a516a33
nahoang <nahoang9698@gmail.com>
2023-05-28 16:14:26 +0700

    feat: Implement edit form

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -17,7 +17,7 @@
 
                 <div class="card-body">
                     @include('layouts._messages')
-                    
+
                     @foreach ($questions as $question)
                           <div class="d-flex flex-column counters">
                             <div class="vote">
@@ -31,9 +31,14 @@
                             </div>
                           </div>
                           <div class="media-body">
-                            <h3 class="mt-0">
-                              <a href="{{ $question->url }}"> {{ $question->title }} </a>
-                            </h3>
+                            <div class="d-flex align-items-center">
+                              <h3 class="mt-0">
+                                <a href="{{ $question->url }}"> {{ $question->title }} </a>
+                              </h3>
+                              <div class="ml-auto">
+                                <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
+                              </div>
+                            </div>
                             <p class="lead">
                               Asked by 
                               <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
