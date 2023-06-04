7903f2de6f87da7400d1dded5f003ae129bd481f
nahoang <nahoang9698@gmail.com>
2023-06-04 15:14:35 +0700

    feat: Add vote controls to question and answer item

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -5,30 +5,46 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">
-                    <div class="d-flex align-items-center">
-                      <h1>{{ $question->title }}</h1>
-                    <div class="ml-auto">
-                      <a href="{{ route('questions.create') }}" class="btn btn-outline-secondary">Ask Question</a>
-                    </div> 
-                  </div>
-                  
+               <div class="card-body">
+                <div class="card-title">
+                  <div class="d-flex align-items-center">
+                    <h1>{{ $question->title }}</h1>
+                  <div class="ml-auto">
+                    <a href="{{ route('questions.create') }}" class="btn btn-outline-secondary">Ask Question</a>
+                  </div> 
                 </div>
+              </div>
 
-                <div class="card-body">
-                 {!! $question->body_html !!}
+              <hr>
+              <div class="media">
+                <div class="d-flex flex-column vote-controls">
+                  <a href="" title="This question is useful" class="vote-up">
+                    <i class="fas fa-caret-up fa-3x"></i>
+                  </a>
+                  <span class="votes-count">1230</span>
+                  <a href="" title="This question is not useful" class="vote-down off">
+                    <i class="fas fa-caret-down fa-3x"></i>
+                  </a>
+                  <a  title="Click to mark as favorite question (Click again to undo)" class="favorite mt-2 favorited">
+                    <span class="fas fa-star fa-2x">123</span>
+                  </a>
+                </div>
+                <div class="media-body">
+                  {!! $question->body_html !!}
 
-                 <div class="float-right">
-                  <span class="text-muted">Answered {{ $question->created_date }}</span>
-                  <div class="media mt-2">
-                    <a href="{{ $question->user->url }}" class="pr-2">
-                      <img src="{{ $question->user->avatar }}">
-                    </a>
-                    <div class="media-body mt-1">
-                      <a href="{{ $question->user->url  }}">{{ $question->user->name }}</a>
-                    </div>
+               <div class="float-right">
+                <span class="text-muted">Answered {{ $question->created_date }}</span>
+                <div class="media mt-2">
+                  <a href="{{ $question->user->url }}" class="pr-2">
+                    <img src="{{ $question->user->avatar }}">
+                  </a>
+                  <div class="media-body mt-1">
+                    <a href="{{ $question->user->url  }}">{{ $question->user->name }}</a>
                   </div>
                 </div>
+                </div>
+              </div>
+               </div>
             </div>
         </div>
     </div>
@@ -42,6 +58,18 @@
             <hr>
             @foreach ($question->answers as $answer)
                 <div class="media">
+                  <div class="d-flex flex-column vote-controls">
+                    <a href="" title="This answer is useful" class="vote-up">
+                      <i class="fas fa-caret-up fa-3x"></i>
+                    </a>
+                    <span class="votes-count">1230</span>
+                    <a href="" title="This answer is not useful" class="vote-down off">
+                      <i class="fas fa-caret-down fa-3x"></i>
+                    </a>
+                    <a  title="Mark this answer as best answer" class="vote-accepted mt-2">
+                      <span class="fas fa-check fa-2x"></span>
+                    </a>
+                  </div>
                   <div class="media-body">
                     {!! $answer->body_html !!}
                     <div class="float-right">
